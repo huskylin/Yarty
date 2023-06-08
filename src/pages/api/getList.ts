703f56ccff648d010daadea161d399b8835baaa0
huskylin <hank84112092@google.com>
2023-06-09 00:25:14 +0800

    chore: coding style

--- src/pages/api/getList.ts
+++ src/pages/api/getList.ts
@@ -2,13 +2,15 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 const apiPath = process.env.API_PATH;
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<any>) {
-    const getItems = await fetch(
+    const getItems = fetch(
         `${apiPath}/api/getListItems?playlistId=${req.query.playlistId}`
     );
-    const getListName = await fetch(
+    const getListName = fetch(
         `${apiPath}/api/getListName?playlistId=${req.query.playlistId}`
     );
-    const [items, listName] = await Promise.all([getItems.json(), getListName.json()]);
+    const [itemsResponse, listNameResponse] = await Promise.all([getItems, getListName]);
+    const items = await itemsResponse.json();
+    const listName = await listNameResponse.json();
 
     res.end(JSON.stringify({ items, listName }));
 };
