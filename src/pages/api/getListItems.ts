1bfaa82e9cae79207831da535492bfd7a4a20210
huskylin <hank84112092@google.com>
2023-06-13 17:56:37 +0800

    fix: reduce results limit

--- src/pages/api/getListItems.ts
+++ src/pages/api/getListItems.ts
@@ -3,7 +3,7 @@ import fetchYT from './fetchYT';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<any>) {
     const apiName = `playlistItems`
-    const parms = `part=id&part=snippet&maxResults=50&playlistId=${req.query.playlistId}`
+    const parms = `part=id&part=snippet&maxResults=20&playlistId=${req.query.playlistId}`
     const playListItems = await fetchYT(apiName, parms);
 
     res.end(JSON.stringify(playListItems))
