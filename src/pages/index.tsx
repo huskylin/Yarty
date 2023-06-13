95280bff7c37af47815847f3d57481c138cdfbdf
huskylin <hank84112092@google.com>
2023-06-14 01:42:36 +0800

    chore: update meta

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -34,6 +34,7 @@ export default function Home() {
           >
             <Head>
               <title>Yarty - Party Playlist</title>
+              <meta name="description" content="YouTube playlists mixer for party"></meta>
             </Head>
             <div
               style={{
